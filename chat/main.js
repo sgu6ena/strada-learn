1333dd4d7069b2b6d860fe969523c69a3133d4aa
parcanec <igarek200706@gmail.com>
2022-11-26 10:54:41 +0200

    авторизация + смена имени

--- chat/main.js
+++ chat/main.js
@@ -1,14 +1,10 @@
+import Cookies from "js-cookie"
+
 const chat = document.querySelector(".middle");
 chat.scrollTop = chat.scrollHeight;
 const message = document.querySelector('#enter_message')
 
-//Настройки - модалка
-const settingsButton = document.querySelector("#settings")
-const settingsModal = document.querySelector("#settingsModal")
-const settingsModalCloseButton = document.querySelector("#settingsModal").querySelector("#close")
-settingsButton.addEventListener('click', () => settingsModal.classList.add('active'))
-settingsModalCloseButton.addEventListener('click', () => settingsModal.classList.remove('active'))
-
+/////////////////////////
 //Авторизация - модалка
 const autorizationButton = document.querySelector("#autorization")
 const autorizationModal = document.querySelector("#autorizationModal")
@@ -19,6 +15,7 @@ const email = document.querySelector('#enter_email')
 const sendCode = document.querySelector('#email')
 sendCode.addEventListener('submit', sendAuth)
 
+/////////////////////////
 //Авторизация - функция
 async function sendAuth(env){
     env.preventDefault()
@@ -37,14 +34,65 @@ async function sendAuth(env){
     return data
 }
 
+/////////////////////////
 //Подтверждение - модалка
 const confirmationButton = document.querySelector("#confirmation")
 const confirmationModal = document.querySelector("#confirmationModal")
 const confirmationModalCloseButton = document.querySelector("#confirmationModal").querySelector("#close")
 confirmationButton.addEventListener('click', () => confirmationModal.classList.add('active'))
 confirmationModalCloseButton.addEventListener('click', () => confirmationModal.classList.remove('active'))
+const token = document.querySelector('#enter_confirm')
+const checkToken = document.querySelector('#email_confirm')
+//Подтверждение - сохранение токена в куки
+checkToken.addEventListener('submit', () => Cookies.set(`token`, `${token.value}`, { expires: 7 }))
 
-//Авторизация
+/////////////////////////
+//Настройки - модалка
+const settingsButton = document.querySelector("#settings")
+const settingsModal = document.querySelector("#settingsModal")
+const settingsModalCloseButton = document.querySelector("#settingsModal").querySelector("#close")
+settingsButton.addEventListener('click', () => settingsModal.classList.add('active'))
+settingsModalCloseButton.addEventListener('click', () => settingsModal.classList.remove('active'))
+const userName = document.querySelector('#enter_name')
+const sendName = document.querySelector('#name')
+sendName.addEventListener('submit', changeName)
+
+
+/////////////////////////
+//Настройки - смена имени
+async function changeName(env){
+    env.preventDefault()
+    const authUrl = 'https://edu.strada.one/api/user'
+    const namelValue = userName.value
+    let response = await fetch(authUrl, {
+        method: 'PATCH',
+        body: JSON.stringify({name: namelValue}),
+        headers: {
+            'Content-Type': 'application/json',
+            Accept : "application/json",
+            mode : 'no-cors',
+            Authorization : `Bearer ${Cookies.get('token')}`
+        },
+    })
+    const data = await response.json();
+    return data
+}
+
+async function checkUser() {
+    const checkUrl = 'https://edu.strada.one/api/user/me'
+    let response = await fetch(checkUrl, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept : "application/json",
+            mode : 'no-cors',
+            Authorization : `Bearer ${Cookies.get('token')}`
+        },
+    })
+    const data = await response.json();
+    return data
+}
+console.log(checkUser())
 
 const createMessage = (isOutgingMessage, text, time) =>{
     const template = document.querySelector(isOutgingMessage ? '#outgoing_message' : '#incoming_message');
